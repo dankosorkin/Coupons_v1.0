dd2e6d2106cec546ed247a64de9f24ebd9e45388
Daniel Sorkin <danielk.sorkin@gmail.com>
2021-01-12 18:04:30 +0200

    db config

--- Coupons_v1.0/src/util/sql/DB_Config.java
+++ Coupons_v1.0/src/util/sql/DB_Config.java
@@ -1,16 +1,66 @@
 package util.sql;
 
+/**
+ * Database configuration file. Default connection to MySQL server.
+ */
 public class DB_Config {
 
-	private String driver = "";
-	private String protocol = "jdbc:mysql://";
-	private String host = "127.0.0.1";
-	private String port = ":3306";
-	private String db_name = "/Coupons_v1";
-	private String parametres = "?createDatabaseIfNotExist=true&serverTimezone=UTC";
-	private String url =protocol+host+port+db_name+parametres ;
+	// driver and connection
+	private final static String DRIVER = "com.mysql.cj.jdbc.Driver.class";
+	private final static String PROTOCOL = "jdbc:mysql://";
+	private final static String HOST = "127.0.0.1";
+	private final static String PORT = ":3306";
+	private final static String DB = "/Coupons_v1";
+	private final static String PARAMS = "?createDatabaseIfNotExist=true&serverTimezone=UTC";
+	private final static String URL = PROTOCOL + HOST + PORT + DB + PARAMS;
 
-	private String user = "root";
-	private String password = "pass1";
+	// user credentials
+	private final static String USER = "root";
+	private final static String PASS = "pass1";
+
+	/**
+	 * Static method returns driver
+	 * 
+	 * @return String DRIVER
+	 */
+	public static String getDriver() {
+		return DRIVER;
+	}
+
+	/**
+	 * Static method returns database name
+	 * 
+	 * @return String
+	 */
+	public static String getDb_name() {
+		return DB;
+	}
+
+	/**
+	 * Static method returns connection URL
+	 * 
+	 * @return String
+	 */
+	public static String getUrl() {
+		return URL;
+	}
+
+	/**
+	 * Static method returns user name
+	 * 
+	 * @return String
+	 */
+	public static String getUser() {
+		return USER;
+	}
+
+	/**
+	 * Static method returns user password
+	 * 
+	 * @return String
+	 */
+	public static String getPassword() {
+		return PASS;
+	}
 
 }
