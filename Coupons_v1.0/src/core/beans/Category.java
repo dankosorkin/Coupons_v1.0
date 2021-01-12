8f5ef1fe06c86e0893acef471a7dc39e1417aa1b
Daniel Sorkin <danielk.sorkin@gmail.com>
2021-01-12 16:46:59 +0200

    Beans classes

---
+++ Coupons_v1.0/src/core/beans/Category.java
@@ -0,0 +1,12 @@
+package core.beans;
+
+/**
+ * @autor Daniel Sorkin
+ * 
+ *        last update 2021-1-11
+ */
+public enum Category {
+
+	FOOD, ELECTRICITY, RESTAURANT, VACATION, GAMES, SPORT;
+
+}
