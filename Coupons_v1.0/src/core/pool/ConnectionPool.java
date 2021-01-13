68555d39afe7b03aad7d27f15c63809b58c7c725
Daniel Sorkin <danielk.sorkin@gmail.com>
2021-01-13 22:12:28 +0200

    ConnectionPool docs + custom exception messages added

--- Coupons_v1.0/src/core/pool/ConnectionPool.java
+++ Coupons_v1.0/src/core/pool/ConnectionPool.java
@@ -12,7 +12,7 @@ import util.sql.DB_Config;
 
 /**
  * Singleton class with lazy initialization. ConnectionPool instance manages
- * pool of connections via syncrhonzed methods getConnection() and
+ * pool of connections via synchronized methods getConnection() and
  * restoreConnection().
  */
 public class ConnectionPool {
@@ -26,16 +26,25 @@ public class ConnectionPool {
 	private ConnectionPool() throws ConnectionPoolException {
 		try {
 			Class.forName(DB_Config.getDriver());
+
 			for (int i = 0; i < POOL_SIZE; i++)
 				pool.add(DriverManager.getConnection(DB_Config.getUrl(), DB_Config.getUser(), DB_Config.getPassword()));
 		} catch (SQLException e) {
-			throw new ConnectionPoolException("", e);
+			throw new ConnectionPoolException(
+					"[x] CoonectionPool CTOR -> there is a problem to add connection to the pool", e);
 		} catch (ClassNotFoundException e) {
-			throw new ConnectionPoolException("There is a problem to load database driver", e);
+			throw new ConnectionPoolException("[x] CoonectionPool CTOR -> there is a problem to load database driver",
+					e);
 		}
 
 	}
 
+	/**
+	 * Static method returns single instance of ConnectionPool on every call of the
+	 * method
+	 * 
+	 * @throws ConnectionPoolException
+	 */
 	public static ConnectionPool getInstance() throws ConnectionPoolException {
 		if (instance == null)
 			instance = new ConnectionPool();
@@ -43,13 +52,21 @@ public class ConnectionPool {
 		return instance;
 	}
 
+	/**
+	 * Synchronized method remove one connection from the pool. In case of empty
+	 * pool the method calls to method wait() for caller thread.
+	 * 
+	 * @throws ConnectionPoolException
+	 */
 	public synchronized Connection getConnection() throws ConnectionPoolException {
 
+		// if pool is empty -> wait
 		while (pool.isEmpty()) {
 			try {
 				wait();
 			} catch (InterruptedException e) {
-				throw new ConnectionPoolException("POOL", e);
+				throw new ConnectionPoolException(
+						"[x] CoonectionPool.getConnection() -> waiting for a connection were interrupted", e);
 			}
 		}
 
@@ -60,12 +77,21 @@ public class ConnectionPool {
 		return conn;
 	}
 
+	/**
+	 * Synchronized method returns a connection to the pool and notifies waiting
+	 * threads
+	 */
 	public synchronized void restoreConnection(Connection connection) {
 		pool.add(connection);
 		notifyAll();
 	}
 
-	public synchronized void closeAllConnections() {
+	/**
+	 * Synchronized method close all connections and release memory resource.
+	 * 
+	 * @throws ConnectionPoolException
+	 */
+	public synchronized void closeAllConnections() throws ConnectionPoolException {
 
 		int counter = 0;
 
@@ -75,7 +101,8 @@ public class ConnectionPool {
 				try {
 					wait();
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					throw new ConnectionPoolException(
+							"[x] CoonectionPool.closeAllConnections() -> waiting for a connection were interrupted", e);
 				}
 			}
 
@@ -86,7 +113,8 @@ public class ConnectionPool {
 					it.next().close();
 					it.remove();
 				} catch (SQLException e) {
-					e.printStackTrace();
+					throw new ConnectionPoolException(
+							"[x] CoonectionPool.closeAllConnections() -> there is a problem to close connection", e);
 				}
 
 				counter++;
